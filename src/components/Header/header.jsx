3bfd14b871209c3a0db99bd719c840971b9775fa
EmanoelCarlosBarreto <emanoelcarlos609@gmail.com>
2024-12-02 19:37:30 -0300

    fix: image import

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import LogoLivro from '../../assets/Logolivro.png';
+import LogoLivro from '../../assets/LogoLivro.png';
 import Lupa from '../../assets/Lupa.png';
 import Inicio from '../../pages/inicio/Inicio';
 import LivrosDoados from '../../pages/livrosDoados/LivrosDoados';
